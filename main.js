b8aa398be916691b5bc8f7b625788306ae27cc9c
roipicsou <pl613330@gmail.com>
2025-07-12 18:21:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+/*
+
+fonctionalitéer a ajouter :
+  - nouriture
+  - deplacement
+  - cycle jour/nuit
+  - création de desandace
+  - modification a la nécance
+
+*/
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+class Creature {
+    constructor(){
+        this.x = 10;
+        this.y = 50;
+        this.radius = 5;
+        this.color = 'red';
+    }
+
+    draw(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+var test = new Creature();
+test.draw(ctx);
