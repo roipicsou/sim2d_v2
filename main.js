90e4f6998dbeef94bb538825939858f6dbf36efa
roipicsou <pl613330@gmail.com>
2025-07-16 11:26:03 +0200

    deplacement

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ class Creature {
         this.y = 50;
         this.radius = 5;
         this.color = 'red';
+        this.speed = 2;
     }
 
     draw(ctx) {
@@ -24,7 +25,42 @@ class Creature {
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fillStyle = this.color;
     ctx.fill();
-  }
+    }
+
+    find_food(Foods) {
+      let food = null;
+
+      for (let item of Foods) {
+        const dx = item.x - this.x;
+        const dy = item.y - this.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        console.log(distance);
+
+        if (!food || distance < Math.sqrt((food.x - this.x) ** 2 + (food.y - this.y) ** 2)) {
+          food = item;
+        }
+      }
+      return food;
+    }
+
+    move(Foods) {
+    const closest = this.find_food(Foods)
+
+    if (closest) {
+      const dx = closest.x - this.x;
+      const dy = closest.y - this.y;
+      const dist = Math.sqrt(dx * dx + dy * dy);
+      const directionX = dx / dist;
+      const directionY = dy / dist;
+
+      this.x += directionX * this.speed;
+      this.y += directionY * this.speed;
+    } else {
+      this.x += (Math.random() - 0.5) * this.speed * 2;
+      this.y += (Math.random() - 0.5) * this.speed * 2;
+    }
+
+    }
 }
 
 class Food {
@@ -43,10 +79,16 @@ class Food {
   }
 }
 
+const Creatures = [];
+const Foods = [];
+
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-var test = new Creature();
-test.draw(ctx);
+Creatures.push(new Creature());
+Foods.push(new Food());
 
-var test_food = new Food();
-test_food.draw(ctx);
+const test1 = Creatures[0];
+test1.draw(ctx);
+
+const test2 = Foods[0];
+test2.draw(ctx);
